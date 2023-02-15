91c70dfc589586a63bbad701a9e5191876ee3b56
Jufri Ali <faqat0_0@hotmail.com>
2023-02-15 21:43:52 +0800

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(
+  name: 'Chinese Restaurant',
+  address: '123 Chinatown Ave, Atlanta',
+  phone_number: '88776833',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: 'Italian Restaurant',
+  address: '43 La Vida Street, Chicago',
+  phone_number: '32345323',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'Japanese Restaurant',
+  address: '444 Shinojen Avenue, Osaka',
+  phone_number: '87654362',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'French Restaurant',
+  address: '76 Mistral Side Road, France',
+  phone_number: '89101112',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'Belgian Restaurant',
+  address: '556 Windy Road, Belgium',
+  phone_number: '1234567',
+  category: 'belgian'
+)
